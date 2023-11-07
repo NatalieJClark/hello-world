867a57b07471e107a5d099db4e11613fc121b0d3
Natalie Clark <nj_clark@hotmail.co.uk>
2023-11-07 10:03:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Hello World Web Project
+
+## Introduction
+
+- This is my Hello World Web Project for Makers SWE Specialism Module 1 - Learning Java & Its Tools
+
+## Aims
+
+- [x] Learn how to set up a Spring Boot app
+- [x] Learn how to agg templates to my web app with Thymeleaf
---
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        return new ModelAndView("home");
+    }
+
+    @GetMapping("/links")
+    public ModelAndView links() {
+        return new ModelAndView("links");
+    }
+
+    @GetMapping("/quotes")
+    public ModelAndView quotes() {
+        return new ModelAndView("quotes");
+    }
+}
